c7bcf0bc200edc4cf6d58af0f4fb076323738501
NVR1282 <011045@mljh.ylc.edu.tw>
2024-12-06 16:27:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://nvr1282.github.io/frog11324130/model.json';
 
 
 function preload() {
